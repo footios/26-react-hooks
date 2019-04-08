65c19d255da1b7ddf8b03f021604fcf2353a79d9
Fotis Tsakiris <footios76@gmail.com>
2019-04-08 12:01:45 +0300

    462. useEffect to fetch todos from server

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,47 +1,105 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios'
 
 const todo = (props) => {
+	/* RULES
+    You must set the Hooks, only at the top-level of your function! */
+
 	/* useState('...') takes an initial state and it returns an array with 2 elements. 
     The 1st is the current state. 
     The 2nd is a function we can use to manipulate that state. */
 
-	// const [ todoName, setTodoName ] = useState('');
-    // const [ todoList, setTodoList ] = useState([]);
+	const [ todoName, setTodoName ] = useState('');
+    const [ todoList, setTodoList ] = useState([]);
     
-    /* Insted of the above code we can merge the states into an object.
+    /* Let's say that we want to load the todos we sent to the server,
+       at the point when this component gets loaded. 
+       
+       We must do it in the `useEffect` hook, because it hooks into React's internals and makes sure
+       that this code executes at the right time 
+       which is after this render cycle finished,
+       so that this can run in a high performant way 
+       and that the UI is always updated correctly 
+       and you don't end up with some strange state changes 
+       behind the scenes outside of what React expected.
+
+       So we added the `useEffect` hook to cause side effects,
+       it worked but we caused quite a big side effect 
+       because we entered an infinite loop.
+       The reason for that is that useEffect does not only run once, 
+       like for example componentDidMount did
+       but it runs after every render cycle.
+       `useEffect`, takes a second argument, which is an array of values.
+       If one of these values changes only then useEffect runs.
+       If you put an empty array, you copy componentDidMout,
+       because it runs only once.
+
+*/
+
+
+    useEffect(() => {
+        axios.get('https://todolist-58f53.firebaseio.com/todos.json')
+            .then(res => {
+                console.log(res);
+                const todoData = res.data;
+                const todos = []
+                for (const key in todoData) {
+                    if (todoData.hasOwnProperty(key)) {
+                        todos.push({id: key, name: todoData[key].name});
+                    }
+                }
+                setTodoList(todos)
+                
+            })
+            .catch(err => {
+                console.log(err);
+                
+            })
+    }, [todoName])
+
+	/* Insted of the above code we can merge the states into an object.
     This though is not the optimal approach here,
     because we have to update every property in the object everytime. */
 
-    const [todoState, setTodoState] = useState({userInput: '', todoList: []})
+	// const [todoState, setTodoState] = useState({userInput: '', todoList: []})
+
+	const inputStateHandler = (event) => {
+		setTodoName(event.target.value);
+	};
 
 	// const inputStateHandler = (event) => {
-	// 	setTodoName(event.target.value);
-    // };
-    
-    const inputStateHandler = (event) => {
-        setTodoState({userInput: event.target.value, todoList: todoState.todoList})
-    }
+	//     setTodoState({userInput: event.target.value, todoList: todoState.todoList})
+	// }
+
+	const todoAddHandler = () => {
+        setTodoList(todoList.concat(todoName));
+        axios.post('https://todolist-58f53.firebaseio.com/todos.json', {name: todoName})
+        .then(res => {
+            console.log(res);
+            
+        })
+        .catch(err => {
+            console.log(err);
+            
+        })
+	};
 
 	// const todoAddHandler = () => {
-	// 	setTodoList(todoList.concat(todoName))
-    // };
-    
-    const todoAddHandler = () => {
-        setTodoState({
-            userInput: todoState.userInput,
-            todoList: todoState.todoList.concat(todoState.userInput)
-        })
-    }
-
+	//     setTodoState({
+	//         userInput: todoState.userInput,
+	//         todoList: todoState.todoList.concat(todoState.userInput)
+	//     })
+	// }
 
 	/* With React.Fragment we can have top level siblings */
 	return (
 		<React.Fragment>
-			<input type="text" placeholder="Todo" onChange={inputStateHandler} value={todoState.userInput} />
+			<input type="text" placeholder="Todo" onChange={inputStateHandler} value={todoName} />
 			<button type="button" onClick={todoAddHandler}>
 				Add
 			</button>
-			<ul>{todoState.todoList.map((todo, index) => <li key={index}>{todo}</li>)}</ul>
+			<ul>{todoList.map((todo) => <li key={todo.id}>{todo.name}</li>)}</ul>
 		</React.Fragment>
 	);
 };
