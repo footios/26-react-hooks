736e5599af4b42f5cb972e55057d07235bb9be56
Fotis Tsakiris <footios76@gmail.com>
2019-04-08 09:38:07 +0300

    456. destructuring added to what useState returns

---
+++ README.md
@@ -0,0 +1,7 @@
+[React 16.6 - The Complete Guide (incl. React Router & Redux)](https://www.udemy.com/react-the-complete-guide-incl-redux/)
+
+> Section 26
+
+> Bonus: React Hooks
+
+> Lectures 452 - 474
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,15 +4,16 @@ const todo = props => {
     /* useState('...') takes an initial state and it returns an array with 2 elements. 
     The 1st is the current state. 
     The 2nd is a function we can use to manipulate that state. */
-const inputState =  useState('')
+
+const [todoName, setTodoName] =  useState('')
 
 const inputStateHandler = (event) => {
-    inputState[1](event.target.value)
+    setTodoName(event.target.value)
 }
 
     /* With React.Fragment we can have top level siblings */
     return <React.Fragment> 
-<input type='text' placeholder='Todo' onChange={inputStateHandler} value={inputState[0]} />
+<input type='text' placeholder='Todo' onChange={inputStateHandler} value={todoName} />
 <button type='button'>Add</button>
 <ul>
 
