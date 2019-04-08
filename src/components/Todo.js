f9c5c0ab6221f9c977a61d0124092c70d5586bdc
Fotis Tsakiris <footios76@gmail.com>
2019-04-08 11:00:40 +0300

    458. Using one state that takes an object

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,25 +5,43 @@ const todo = (props) => {
     The 1st is the current state. 
     The 2nd is a function we can use to manipulate that state. */
 
-	const [ todoName, setTodoName ] = useState('');
-	const [ todoList, setTodoList ] = useState([]);
+	// const [ todoName, setTodoName ] = useState('');
+    // const [ todoList, setTodoList ] = useState([]);
+    
+    /* Insted of the above code we can merge the states into an object.
+    This though is not the optimal approach here,
+    because we have to update every property in the object everytime. */
 
-	const inputStateHandler = (event) => {
-		setTodoName(event.target.value);
-	};
+    const [todoState, setTodoState] = useState({userInput: '', todoList: []})
 
-	const todoAddHandler = () => {
-		setTodoList(todoList.concat(todoName))
-	};
+	// const inputStateHandler = (event) => {
+	// 	setTodoName(event.target.value);
+    // };
+    
+    const inputStateHandler = (event) => {
+        setTodoState({userInput: event.target.value, todoList: todoState.todoList})
+    }
+
+	// const todoAddHandler = () => {
+	// 	setTodoList(todoList.concat(todoName))
+    // };
+    
+    const todoAddHandler = () => {
+        setTodoState({
+            userInput: todoState.userInput,
+            todoList: todoState.todoList.concat(todoState.userInput)
+        })
+    }
+
 
 	/* With React.Fragment we can have top level siblings */
 	return (
 		<React.Fragment>
-			<input type="text" placeholder="Todo" onChange={inputStateHandler} value={todoName} />
+			<input type="text" placeholder="Todo" onChange={inputStateHandler} value={todoState.userInput} />
 			<button type="button" onClick={todoAddHandler}>
 				Add
 			</button>
-			<ul>{todoList.map((todo, index) => <li key="index">{todo}</li>)}</ul>
+			<ul>{todoState.todoList.map((todo, index) => <li key={index}>{todo}</li>)}</ul>
 		</React.Fragment>
 	);
 };
